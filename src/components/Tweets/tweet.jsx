647184b3b9d711134299f035f822dd1bee64541d
Kinkela <gedeon.kinkela@kadea.local>
2023-11-24 14:00:34 +0100

    tweet action

--- src/components/Tweets/tweet.jsx
+++ src/components/Tweets/tweet.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import tweetprofilephoto from "./../../images/tweet-profile-photo.png"
 import tweetVector from "./../../images/Vector.svg"
-import TweetActions from "../Tweets/tweetactions"
+import reply from "./../../images/Reply.svg"
+import Retweet from "./../../images/Retweet.svg"
+import react from "./../../images/React.svg"
+import TweetReplies from "./../../images/Tweet-Replies.svg"
 import Tweet2 from "./tweet2";
 // import Tweet1 from "./tweet";
 function Tweet() {
@@ -23,12 +26,16 @@ function Tweet() {
                      <div className="tweet-text">
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid repellat mollitia eligendi eius quibusdam saepe ipsum consequuntur voluptate distinctio et vitae, provident quas sint sit deleniti laboriosam officiis veritatis nihil.</p>
                      </div>
-                <TweetActions />
+                        <div className='tweet-editor-actions'>
+                            <span className="tweet-action"><img src={reply} alt="" srcset="" />255</span>
+                            <span className="tweet-action"><img src={Retweet} alt="" srcset="" />1455</span>
+                            <span className="tweet-action"><img src={react} alt="" srcset="" />5550</span>
+                            <span className="tweet-action"><img src={TweetReplies} alt="" srcset="" /></span>
+                        </div>
             </div>
         </div> 
     </div>
      <Tweet2 />
-     {/* <Tweet1 /> */}
 </div>
     )
 }
--- src/components/Tweets/tweetactions.jsx
+++ src/components/Tweets/tweetactions.jsx
@@ -6,10 +6,10 @@ import TweetReplies from "./../../images/Tweet-Replies.svg"
 function TweetActions() {
     return(
     <div className='tweet-editor-actions'>
-        <img className="tweet-action" src={reply} alt="" srcset="" />
-        <img className="tweet-action" src={Retweet} alt="" srcset="" />
-        <img className="tweet-action" src={react} alt="" srcset="" />
-        <img className="tweet-action" src={TweetReplies} alt="" srcset="" />
+        <span className="tweet-action"><img src={reply} alt="" srcset="" />255</span>
+        <span className="tweet-action"><img src={Retweet} alt="" srcset="" />1000</span>
+        <span className="tweet-action"><img src={react} alt="" srcset="" />5550</span>
+        <span className="tweet-action"><img src={TweetReplies} alt="" srcset="" /></span>
     </div>
     )  
 }
